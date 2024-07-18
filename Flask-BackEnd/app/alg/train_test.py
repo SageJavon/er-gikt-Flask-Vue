d20962d22a10443982a14f65ea0b2eaaf7b77b1f
赵晗瑜 <2038290448@qq.com>
2024-07-18 08:48:21 +0800

    add 20 epoch and 30 epoch

--- Flask-BackEnd/app/alg/train_test.py
+++ Flask-BackEnd/app/alg/train_test.py
@@ -18,13 +18,13 @@ from utils import gen_gikt_graph, build_adj_list
 
 os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 time_now = datetime.now().strftime('%Y_%m_%d#%H_%M_%S')
-output_path = os.path.join('output', time_now)
+output_path = 'output'
 output_file = open(output_path, 'w')
 # 训练时的超参数
 params = {
     'max_seq_len': max_seq_len,
     'min_seq_len': min_seq_len,
-    'epochs': 2, # 每折训练的轮数
+    'epochs': 20,  # 每折训练的轮数
     'lr': 0.01,
     'lr_gamma': 0.95,
     'batch_size': 32,
@@ -45,11 +45,13 @@ output_file.write(str(params) + '\n')
 print(params)
 batch_size = params['batch_size']
 # 构建模型需要的数据结构, 全部转化为正确类型tensor再输入模型中
-qs_table = torch.tensor(sparse.load_npz('data/qs_table.npz').toarray(), dtype=torch.int64, device=DEVICE)  # [num_q, num_c]
+qs_table = torch.tensor(sparse.load_npz(
+    'data/qs_table.npz').toarray(), dtype=torch.int64, device=DEVICE)  # [num_q, num_c]
 num_question = torch.tensor(qs_table.shape[0], device=DEVICE)
 num_skill = torch.tensor(qs_table.shape[1], device=DEVICE)
 q_neighbors_list, s_neighbors_list = build_adj_list()
-q_neighbors, s_neighbors = gen_gikt_graph(q_neighbors_list, s_neighbors_list, params['size_q_neighbors'], params['size_s_neighbors'])
+q_neighbors, s_neighbors = gen_gikt_graph(
+    q_neighbors_list, s_neighbors_list, params['size_q_neighbors'], params['size_s_neighbors'])
 q_neighbors = torch.tensor(q_neighbors, dtype=torch.int64, device=DEVICE)
 s_neighbors = torch.tensor(s_neighbors, dtype=torch.int64, device=DEVICE)
 
@@ -62,7 +64,7 @@ model = GIKT(
     hard_recap=params['hard_recap'],
 ).to(DEVICE)
 
-loss_fun = torch.nn.BCEWithLogitsLoss().to(DEVICE) # 损失函数
+loss_fun = torch.nn.BCEWithLogitsLoss().to(DEVICE)  # 损失函数
 dataset = UserDataset()  # 数据集
 data_len = len(dataset)  # 数据总长度
 print('model has been built')
@@ -72,10 +74,10 @@ epoch_total = 0
 optimizer = torch.optim.Adam(params=model.parameters(), lr=params['lr'])
 torch.optim.lr_scheduler.ExponentialLR(optimizer, params['lr_gamma'])
 # 在matplotlib中绘制的y轴数据，三行分别表示loss, acc, auc
-y_label_aver = np.zeros([3, params['epochs']]) # 平均精度值
-y_label_all = np.zeros([3, params['epochs'] * params['k_fold']]) # 全部精度值
+y_label_aver = np.zeros([3, params['epochs']])  # 平均精度值
+y_label_all = np.zeros([3, params['epochs'] * params['k_fold']+1])  # 全部精度值
 
-k_fold = KFold(n_splits=params['k_fold'] ,shuffle=True) # 交叉验证
+k_fold = KFold(n_splits=params['k_fold'], shuffle=True)  # 交叉验证
 for epoch in range(params['epochs']):
     train_loss_aver = train_acc_aver = train_auc_aver = 0
     test_loss_aver = test_acc_aver = test_auc_aver = 0
@@ -85,15 +87,18 @@ for epoch in range(params['epochs']):
         train_set = Subset(dataset, train_indices)  # 训练集
         test_set = Subset(dataset, test_indices)  # 测试集
         if DEVICE.type == 'cpu':  # Cpu(本机)
-            train_loader = DataLoader(train_set, batch_size=batch_size)  # 训练数据加载器
-            test_loader = DataLoader(test_set, batch_size=batch_size)  # 测试数据加载器
+            train_loader = DataLoader(
+                train_set, batch_size=batch_size)  # 训练数据加载器
+            test_loader = DataLoader(
+                test_set, batch_size=batch_size)  # 测试数据加载器
         else:  # Gpu(服务器)
             train_loader = DataLoader(train_set, batch_size=batch_size, num_workers=params['num_workers'],
                                       pin_memory=True, prefetch_factor=params['prefetch_factor'])
             test_loader = DataLoader(test_set, batch_size=batch_size, num_workers=params['num_workers'],
                                      pin_memory=True, prefetch_factor=params['prefetch_factor'])
         train_data_len, test_data_len = len(train_set), len(test_set)
-        print('===================' + LOG_Y + f'epoch: {epoch_total + 1}'+ LOG_END + '====================')
+        print('===================' + LOG_Y +
+              f'epoch: {epoch_total + 1}' + LOG_END + '====================')
 
         # 训练阶段，既有前向传播，也有反向传播
         print('-------------------training------------------')
@@ -103,7 +108,8 @@ for epoch in range(params['epochs']):
         for data in train_loader:
             # 梯度清零
             optimizer.zero_grad()
-            x, y_target, mask = data[:, :, 0].to(DEVICE), data[:, :, 1].to(DEVICE), data[:, :, 2].to(torch.bool).to(DEVICE)
+            x, y_target, mask = data[:, :, 0].to(DEVICE), data[:, :, 1].to(
+                DEVICE), data[:, :, 2].to(torch.bool).to(DEVICE)
             # 从data取出来时，mask的类型是int而不是bool
             y_hat = model(x, y_target, mask)
             y_hat = torch.masked_select(y_hat, mask)
@@ -121,7 +127,8 @@ for epoch in range(params['epochs']):
             loss.backward()
             optimizer.step()
             train_step += 1
-            print(f'step: {train_step}, loss: {loss.item():.4f}, acc: {acc.item():.4f}, auc: {auc:.4f}')
+            print(
+                f'step: {train_step}, loss: {loss.item():.4f}, acc: {acc.item():.4f}, auc: {auc:.4f}')
         train_loss, train_acc = train_loss / train_step, train_right / train_total
         train_loss_aver += train_loss
         train_acc_aver += train_acc
@@ -132,7 +139,8 @@ for epoch in range(params['epochs']):
         test_step = test_loss = test_total = test_right = test_auc = 0
         # 每轮训练第几个批量, 总损失, 训练的真实样本个数, 其中正确的个数, 总体的auc
         for data in test_loader:
-            x, y_target, mask = data[:, :, 0].to(DEVICE), data[:, :, 1].to(DEVICE), data[:, :, 2].to(torch.bool).to(DEVICE)
+            x, y_target, mask = data[:, :, 0].to(DEVICE), data[:, :, 1].to(
+                DEVICE), data[:, :, 2].to(torch.bool).to(DEVICE)
             y_hat = model(x, y_target, mask)
             y_hat = torch.masked_select(y_hat, mask.to(torch.bool))
             y_pred = torch.ge(y_hat, torch.tensor(0.5))
@@ -147,7 +155,8 @@ for epoch in range(params['epochs']):
             auc = roc_auc_score(y_target.cpu(), y_pred.cpu())
             test_auc += auc * len(x) / test_data_len
             test_step += 1
-            print(f'step: {test_step}, loss: {loss.item():.4f}, acc: {acc.item():.4f}, auc: {auc:.4f}')
+            print(
+                f'step: {test_step}, loss: {loss.item():.4f}, acc: {acc.item():.4f}, auc: {auc:.4f}')
         test_loss, test_acc = test_loss / test_step, test_right / test_total
         test_loss_aver += test_loss
         test_acc_aver += test_acc
@@ -157,14 +166,20 @@ for epoch in range(params['epochs']):
         epoch_total += 1
         time1 = time.time()
         run_time = time1 - time0
-        print(LOG_B + f'training: loss: {train_loss:.4f}, acc: {train_acc:.4f}, auc: {train_auc: .4f}' + LOG_END)
-        print(LOG_B + f'testing: loss: {test_loss:.4f}, acc: {test_acc:.4f}, auc: {test_auc: .4f}' + LOG_END)
-        print(LOG_B + f'time: {run_time:.2f}s, average batch time: {(run_time / test_step):.2f}s' + LOG_END)
+        print(
+            LOG_B + f'training: loss: {train_loss:.4f}, acc: {train_acc:.4f}, auc: {train_auc: .4f}' + LOG_END)
+        print(
+            LOG_B + f'testing: loss: {test_loss:.4f}, acc: {test_acc:.4f}, auc: {test_auc: .4f}' + LOG_END)
+        print(
+            LOG_B + f'time: {run_time:.2f}s, average batch time: {(run_time / test_step):.2f}s' + LOG_END)
         # 保存输出至本地文件
         output_file.write(f'epoch {epoch_total} | ')
-        output_file.write(f'training: loss: {train_loss:.4f}, acc: {train_acc:.4f}, auc: {train_auc: .4f} | ')
-        output_file.write(f'testing: loss: {test_loss:.4f}, acc: {test_acc:.4f}, auc: {test_auc: .4f} | ')
-        output_file.write(f'time: {run_time:.2f}s, average batch time: {(run_time / test_step):.2f}s\n')
+        output_file.write(
+            f'training: loss: {train_loss:.4f}, acc: {train_acc:.4f}, auc: {train_auc: .4f} | ')
+        output_file.write(
+            f'testing: loss: {test_loss:.4f}, acc: {test_acc:.4f}, auc: {test_auc: .4f} | ')
+        output_file.write(
+            f'time: {run_time:.2f}s, average batch time: {(run_time / test_step):.2f}s\n')
         # 保存至数组，之后用matplotlib画图
         y_label_all[0][epoch_total], y_label_all[1][epoch_total], y_label_all[2][epoch_total] = test_loss, test_acc, test_auc
     # epoch总结阶段
@@ -174,15 +189,21 @@ for epoch in range(params['epochs']):
     test_loss_aver /= params['k_fold']
     test_acc_aver /= params['k_fold']
     test_auc_aver /= params['k_fold']
-    print('>>>>>>>>>>>>>>>>>>' + LOG_Y + f"epoch: {epoch_total - params['k_fold']} - {epoch_total}"+ LOG_END + '<<<<<<<<<<<<<<<<<<')
-    print(LOG_G + f'training: loss: {train_loss_aver:.4f}, acc: {train_acc_aver:.4f}, auc: {train_auc_aver: .4f}' + LOG_END)
-    print(LOG_G + f'testing: loss: {test_loss_aver:.4f}, acc: {test_acc_aver:.4f}, auc: {test_auc_aver: .4f}' + LOG_END)
-    output_file.write(f"epoch {epoch_total - params['k_fold']} - {epoch_total} | ")
-    output_file.write(f'training: loss: {train_loss_aver:.4f}, acc: {train_acc_aver:.4f}, auc: {train_auc_aver: .4f} | ')
-    output_file.write(f'testing: loss: {test_loss_aver:.4f}, acc: {test_acc_aver:.4f}, auc: {test_auc_aver: .4f}\n')
+    print('>>>>>>>>>>>>>>>>>>' + LOG_Y +
+          f"epoch: {epoch_total - params['k_fold']} - {epoch_total}" + LOG_END + '<<<<<<<<<<<<<<<<<<')
+    print(
+        LOG_G + f'training: loss: {train_loss_aver:.4f}, acc: {train_acc_aver:.4f}, auc: {train_auc_aver: .4f}' + LOG_END)
+    print(
+        LOG_G + f'testing: loss: {test_loss_aver:.4f}, acc: {test_acc_aver:.4f}, auc: {test_auc_aver: .4f}' + LOG_END)
+    output_file.write(
+        f"epoch {epoch_total - params['k_fold']} - {epoch_total} | ")
+    output_file.write(
+        f'training: loss: {train_loss_aver:.4f}, acc: {train_acc_aver:.4f}, auc: {train_auc_aver: .4f} | ')
+    output_file.write(
+        f'testing: loss: {test_loss_aver:.4f}, acc: {test_acc_aver:.4f}, auc: {test_auc_aver: .4f}\n')
     y_label_aver[0][epoch], y_label_aver[1][epoch], y_label_aver[2][epoch] = test_loss_aver, test_acc_aver, test_auc_aver
 
 output_file.close()
-torch.save(model, f=f'model/{time_now}.pt')
-np.savetxt(f'chart_data/{time_now}_all.txt', y_label_all)
-np.savetxt(f'chart_data/{time_now}_aver.txt', y_label_aver)
+torch.save(model, f=f'model/result.pt')
+np.savetxt(f'chart_data/result_all.txt', y_label_all)
+np.savetxt(f'chart_data/result_aver.txt', y_label_aver)
